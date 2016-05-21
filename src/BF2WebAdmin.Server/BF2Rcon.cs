921e3f59d159bc6f064011b7af071d110628635e
nihlen <alex.nihlen@gmail.com>
2016-05-21 03:13:16 +0200

    Added RCON communication (TCP/IP)

---
+++ src/BF2WebAdmin.Server/BF2Rcon.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BF2WebAdmin.Server
+{
+    public class BF2Rcon
+    {
+        private const string DigestSeedResponse = "### Digest seed: ";
+        private const string NotAuthenticatedResponse = "error: not authenticated: you can only invoke 'login'";
+        private const string AuthenticationFailedResponse = "Authentication failed.";
+        private const string AuthenticationSuccessResponse = "Authentication successful, rcon ready.";
+
+        public static string SendCommand(IPAddress ipAddress, int port, string password, string command)
+        {
+            using (var stream = GetStream(ipAddress, port))
+            using (var reader = new StreamReader(stream))
+            using (var writer = new BinaryWriter(stream))
+            {
+                while (true)
+                {
+                    var message = reader.ReadLine();
+                    Console.WriteLine($"> '{message}'");
+                    if (string.IsNullOrWhiteSpace(message))
+                        continue;
+
+                    if (message.StartsWith(DigestSeedResponse))
+                    {
+                        var hash = GetMD5Hash(message.Substring(17), password);
+                        writer.Write(GetLoginBytes("login " + hash));
+                    }
+                    else if (message.StartsWith(NotAuthenticatedResponse))
+                    {
+                        throw new Exception("Not authenticated");
+                    }
+                    else if (message.StartsWith(AuthenticationFailedResponse))
+                    {
+                        throw new Exception("Authentication failed");
+                    }
+                    else if (message.StartsWith(AuthenticationSuccessResponse))
+                    {
+                        Console.WriteLine("Success");
+                        break;
+                    }
+                }
+
+                // Authenticated - Send command
+                writer.Write(GetCommandBytes(command));
+                var response = ReadCommandResponse(stream);
+                Console.WriteLine($"> '{response}'");
+                return response;
+            }
+        }
+
+        private static NetworkStream GetStream(IPAddress ipAddress, int port)
+        {
+            var client = new TcpClient();
+            client.Connect(new IPEndPoint(ipAddress, port));
+            return client.GetStream();
+        }
+
+        private static string GetMD5Hash(string seed, string password)
+        {
+            var hash = ComputeHash(seed, password);
+            var hex = ToHexString(hash);
+            return hex;
+        }
+
+        private static byte[] ComputeHash(string seed, string password)
+        {
+            var bytes = Encoding.UTF8.GetBytes(seed + password);
+            return MD5.Create().ComputeHash(bytes);
+        }
+
+        private static string ToHexString(byte[] bytes)
+        {
+            var hex = BitConverter.ToString(bytes);
+            return hex.Replace("-", "").ToLower();
+        }
+
+        private static byte[] GetLoginBytes(string command)
+        {
+            return Encoding.UTF8.GetBytes(command + "\n");
+        }
+
+        private static byte[] GetCommandBytes(string command)
+        {
+            return Encoding.UTF8.GetBytes("\x02" + command + "\n");
+        }
+
+        private static string ReadCommandResponse(Stream stream)
+        {
+            var sb = new StringBuilder();
+            while (true)
+            {
+                var ch = stream.ReadByte();
+                if ((ch == -1) || (ch == 4))
+                    return sb.ToString();
+
+                sb.Append((char)ch);
+            }
+        }
+    }
+}
--- src/BF2WebAdmin.Server/Program.cs
+++ src/BF2WebAdmin.Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace BF2WebAdmin.Server
     {
         static void Main(string[] args)
         {
+            BF2Rcon.SendCommand(IPAddress.Parse("127.0.0.1"), 4711, "secret", "wa connect");
+            Console.ReadKey();
         }
     }
 }

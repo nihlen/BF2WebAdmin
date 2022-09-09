62d792b35aa9fb58d7e9736ed4575f5cbfc9056e
Alexander Nihlén <alex.nihlen@gmail.com>
2022-09-10 01:55:35 +0200

    Add EF repositories for maps and matches

---
+++ src/BF2WebAdmin.Data/Repositories/MapRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BF2WebAdmin.Data.Abstractions;
+using BF2WebAdmin.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BF2WebAdmin.Data.Repositories;
+
+public class MapRepository : IMapRepository
+{
+    private readonly BF2Context _context;
+
+    public MapRepository(BF2Context context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<MapMod>> GetAsync()
+    {
+        return await _context.MapMods.ToListAsync();
+    }
+
+    public async Task<MapMod> GetAsync(Guid id)
+    {
+        return await _context.MapMods
+            .Include(mm => mm.MapModObjects)
+            .FirstOrDefaultAsync(mm => mm.Id == id);
+    }
+
+    public async Task CreateAsync(MapMod map)
+    {
+        await _context.MapMods.AddAsync(map);
+    }
+
+    public async Task UpdateAsync(MapMod map)
+    {
+        _context.MapMods.Update(map);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        _context.MapMods.Remove(new MapMod {Id = id });
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ src/BF2WebAdmin.Data/Repositories/MatchRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BF2WebAdmin.Data.Abstractions;
+using BF2WebAdmin.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BF2WebAdmin.Data.Repositories;
+
+public class MatchRepository : IMatchRepository
+{
+    private readonly BF2Context _context;
+
+    public MatchRepository(BF2Context context)
+    {
+        _context = context;
+    }
+
+    public async Task<Match> GetMatchAsync(Guid id)
+    {
+        return await _context.Matches
+            .Include(m => m.MatchRounds)
+            .ThenInclude(mr => mr.MatchRoundPlayers)
+            .FirstOrDefaultAsync(m => m.Id == id);
+    }
+
+    public async Task<IEnumerable<Match>> GetMatchesByNewestAsync(int offset, int numberOfRows)
+    {
+        return await _context.Matches
+            .OrderByDescending(m => m.MatchStart)
+            .Skip(offset)
+            .Take(numberOfRows)
+            .ToListAsync();
+    }
+
+    public async Task InsertMatchAsync(Match match)
+    {
+        await _context.Matches.AddAsync(match);
+    }
+
+    public async Task UpdateMatchAsync(Match match)
+    {
+        _context.Matches.Update(match);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task InsertRoundAsync(MatchRound round)
+    {
+        await _context.MatchRounds.AddAsync(round);
+    }
+}
